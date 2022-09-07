7e16884127728c11d26f143a9b0e2e28be2df6b2
ZAMANWAY <sp17bscs0087@maju.edu.pk>
2022-09-06 17:42:45 -0700

    edit_profile_Api

--- lib/screens/Profile/editprofile.dart
+++ lib/screens/Profile/editprofile.dart
@@ -1,13 +1,48 @@
-import 'package:flutter/material.dart';
-import 'package:stande_aero/helper/colors.dart';
+import 'dart:convert';
+import 'dart:io';
 
-class Editprofile extends StatelessWidget {
+import 'package:http/http.dart' as http;
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:stande_aero/helper/colors.dart';
+import 'package:http/http.dart' as http;
+import '../../helper/global.dart';
+
+class Editprofile extends StatefulWidget {
   const Editprofile({Key? key}) : super(key: key);
 
+  @override
+  State<Editprofile> createState() => _EditprofileState();
+}
+
+class _EditprofileState extends State<Editprofile> {
+  TextEditingController name = TextEditingController();
+  TextEditingController email = TextEditingController();
+  TextEditingController phone = TextEditingController();
+  TextEditingController country = TextEditingController();
+  TextEditingController city = TextEditingController();
+  TextEditingController photo = TextEditingController();
+  String apiGlobal = "https://qtdev.the4loop.com/api/";
+  File? imageFile;
   @override
   Widget build(BuildContext context) {
     double res_width = MediaQuery.of(context).size.width;
     double res_height = MediaQuery.of(context).size.height;
+
+    _getFromGallery() async {
+      PickedFile? pickedFile = await ImagePicker().getImage(
+        source: ImageSource.gallery,
+        maxWidth: 2500,
+        maxHeight: 2500,
+      );
+      if (pickedFile != null) {
+        setState(() {
+          imageFile = File(pickedFile.path);
+        });
+      }
+    }
 
     return Scaffold(
       appBar: AppBar(
@@ -60,12 +95,12 @@ class Editprofile extends StatelessWidget {
               padding: const EdgeInsets.fromLTRB(18.0, 8.0, 18.0, 18.0),
               child: Column(
                 children: [
-                  Container(
-                      width: res_width * 0.375,
-                      child: Image.asset(
-                        'assets/slicing/Untitled-51.png',
-                        height: 150,
-                      )),
+                  // Container(
+                  //     width: res_width * 0.375,
+                  //     // child: Image.asset(
+                  //     //   'assets/slicing/Untitled-51.png',
+                  //     //   height: 150,
+                  //     )),
                   // backgroundColor: Colors.red,
 
                   // Positioned(
@@ -76,47 +111,99 @@ class Editprofile extends StatelessWidget {
                   //     icon: const Icon(Icons.add_a_photo),
                   //   ),
                   // ),
-                  profile_textfield(hed: "Name", labelText: "John Doe"),
-                  profile_textfield(hed: "Email", labelText: "John@gmail.com"),
-                  profile_textfield(hed: "Country", labelText: "Country"),
-                  profile_textfield(hed: "City", labelText: "City"),
-                  profile_textfield(
-                      hed: "Phone Number", labelText: "Phone Number"),
-                  profile_textfield(
-                      hed: "Description",
-                      labelText:
-                          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
-                      maxLines: 6),
-                                      SizedBox(
-                  height: res_height * 0.0125,
-                ),
-                GestureDetector(
-                  onTap: () {
-
-                  },
-                  child: Container(
-                    width: res_width * 0.9,
-                    decoration: BoxDecoration(
-                        color: Color(0xffaf8a39),
-                        borderRadius: BorderRadius.all(Radius.circular(7))),
-                    child: Padding(
-                      padding: const EdgeInsets.all(13.0),
-                      child: Center(
-                        child: Text(
-                          'Safe',
-                          style: TextStyle(
+                  Stack(
+                    children: [
+                      Container(
+                        child: imageFile == null
+                            ? CircleAvatar(
+                                backgroundColor: Colors.transparent,
+                                radius: 75,
+                                backgroundImage: AssetImage(
+                                    'assets/slicing/Untitled-31.png'),
+                              )
+                            : CircleAvatar(
+                                backgroundColor: Colors.transparent,
+                                radius: 75,
+                                backgroundImage: FileImage(imageFile!),
+                              ),
+                      ),
+                      Positioned(
+                        right: 10,
+                        bottom: 25,
+                        child: CircleAvatar(
+                          radius: 18,
+                          child: IconButton(
+                            onPressed: () {
+                              _getFromGallery();
+                            },
+                            icon: const Icon(
+                              Icons.camera_alt_outlined,
                               color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 17),
+                              size: 20,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  profile_textfield(
+                    controller: name,
+                    hed: "Name",
+                    labelText: "John Doe",
+                  ),
+                  profile_textfield(
+                    controller: email,
+                    hed: "Email",
+                    labelText: "John@gmail.com",
+                  ),
+                  profile_textfield(
+                    controller: country,
+                    hed: "Country",
+                    labelText: "Country",
+                  ),
+                  profile_textfield(
+                    controller: city,
+                    hed: "City",
+                    labelText: "City",
+                  ),
+                  profile_textfield(
+                    controller: phone,
+                    hed: "Phone Number",
+                    labelText: "Phone Number",
+                  ),
+                  profile_textfield(
+                    hed: "Description",
+                    labelText:
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+                    maxLines: 6,
+                  ),
+                  SizedBox(
+                    height: res_height * 0.0125,
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      width: res_width * 0.9,
+                      decoration: BoxDecoration(
+                          color: Color(0xffaf8a39),
+                          borderRadius: BorderRadius.all(Radius.circular(7))),
+                      child: Padding(
+                        padding: const EdgeInsets.all(13.0),
+                        child: Center(
+                          child: Text(
+                            'Safe',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 17),
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: res_height * 0.0125,
-                ),
-
+                  SizedBox(
+                    height: res_height * 0.0125,
+                  ),
                 ],
               ),
             ),
@@ -125,6 +212,57 @@ class Editprofile extends StatelessWidget {
       ),
     );
   }
+
+  CFA() async {
+    final uri = Uri.parse('https://qtdev.the4loop.com/api/user/profile/update');
+
+    print(uri);
+
+    var sendData = {
+      'name': name.text,
+      'email': email.text,
+      'phone': phone.text,
+      'country': country.text,
+      'city': city.text,
+      'photo': imageFile,
+    };
+
+    var jsonBody = json.encode(sendData);
+
+    final headers = {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': 'Bearer ${globaltoken}',
+    };
+
+    http.Response response = await http.post(
+      uri,
+      headers: headers,
+      body: jsonBody,
+    );
+
+    print(response.statusCode);
+
+    print(response.body);
+    // try {
+    //   var res_data = json.decode(response.body);
+    // } catch (e) {
+    //   log('$e');
+    // }
+    var res_data = json.decode(response.body);
+
+    print(res_data);
+    if (res_data["status"] == true) {
+      // Get.to(() => credit_from2());
+    } else
+      Get.snackbar(
+        'Error',
+        'Wrong Credentials',
+        animationDuration: Duration(seconds: 2),
+        snackPosition: SnackPosition.BOTTOM,
+      );
+
+    return res_data;
+  }
 }
 
 class profile_textfield extends StatelessWidget {
@@ -132,14 +270,20 @@ class profile_textfield extends StatelessWidget {
 
   var maxLines;
 
-  profile_textfield({Key? key, this.labelText, this.hed, this.maxLines})
-      : super(key: key);
+  profile_textfield({
+    TextEditingController? controller,
+    Key? key,
+    this.labelText,
+    this.hed,
+    this.maxLines,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     double res_width = MediaQuery.of(context).size.width;
     double res_height = MediaQuery.of(context).size.height;
 
+    var controller;
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -160,11 +304,13 @@ class profile_textfield extends StatelessWidget {
                 borderRadius: BorderRadius.circular(5),
                 border: Border.all(color: Colors.white)),
             child: TextField(
+              controller: controller,
               maxLines: maxLines,
               decoration: InputDecoration(
                   border: InputBorder.none,
                   hintText: "$labelText",
-                  contentPadding: EdgeInsets.only(left: 10, top: maxLines != null ? 10 : 0)),
+                  contentPadding: EdgeInsets.only(
+                      left: 10, top: maxLines != null ? 10 : 0)),
               // controller: controller,
             ),
           ),
