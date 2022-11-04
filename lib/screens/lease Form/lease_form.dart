0be9ec117197fcb5e8f330efcf95f43b7778798a
qtechdevexperts <haseebugginqt@gmail.com>
2022-11-05 00:55:36 +0500

    UI change lease form

--- lib/screens/lease Form/lease_form.dart
+++ lib/screens/lease Form/lease_form.dart
@@ -8,6 +8,7 @@ class lease_Form extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    print(placeOrderData.toString());
     double res_width = MediaQuery.of(context).size.width;
     double res_height = MediaQuery.of(context).size.height;
     return Container(
@@ -49,7 +50,7 @@ class lease_Form extends StatelessWidget {
             ],
           ),
         ),
-        extendBodyBehindAppBar: true,
+        // extendBodyBehindAppBar: true,
         body: Container(
           width: double.infinity,
           height: double.infinity,
@@ -60,81 +61,235 @@ class lease_Form extends StatelessWidget {
             ),
           ),
           child: SingleChildScrollView(
-            child: SafeArea(
-              child: Column(
-                children: [
-                  Container(
-                    width: res_width * 0.925,
-                    child: Card(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          children: [
-                            Text(
-                              "Engine Stand Agreement",
-                              style: TextStyle(
-                                  fontSize: 15, fontWeight: FontWeight.bold),
-                            ),
-                            SizedBox(
-                              height: res_height * 0.01,
-                            ),
-                            Text(
-                              """This Engine Stand Agreement (the "Agreement") is entered this_____ day of______ 2022 by and between (i) National Aero Stands, LLC, a Texas limited liability company, having its principal place of business at 500 W. 5th Street, Suite 1010, Austin, TX 78701("NAS"), and (ii)_________having its principal place of business at___________.("CUSTOMER"). """,
-                              style: TextStyle(fontSize: 12),
-                            ),
-                            SizedBox(
-                              height: res_height * 0.01,
-                            ),
-                            Text(
-                              "Witnesseth",
-                              style: TextStyle(
-                                  fontSize: 15, fontWeight: FontWeight.bold),
-                            ),
-                            SizedBox(
-                              height: res_height * 0.01,
-                            ),
-                            Text(
-                              """For sufficient consideration the receipt of which is hereby mutually acknowledged, NAS, as owner, hereby agrees to lease one (1) bootstrap capable engine transportation stand (including accompanying hardware) (herein the foregoing collectively referred to as the "Engine Stand"), as more particularly described below, to CUSTOMER and CUSTOMER agrees to lease the Engine Stand from NAS in accordance with the following basic terms and conditions set forth herein:\n\n 1. Description of Engine Stand: Manufacturer:_________ Engine Stand Color:________ Type:________ Serial No.________ Quantity:___ \n\n2. Delivery: The Engine Stand will be delivered to CUSTOMER ExWorks (Incoterms 2010 Edition) NAS's facility, located in_________ ("Delivery Location") by NAS's tender of the Engine Stand to CUSTOMER's common carrier for shipment outside the State of Florida on (the "Delivery Date")___________. -2022 \n\n3. Agreement Term: This Agreement shall commence on the Delivery Date and shall continue until the date that the Engine Stand has been returned to NAS in accordance with the provisions of Section 14 below (the "Agreement Term") \n\n4. Daily Rent: Daily Rent will be charged as follows:___________ U.S. Dollars (US.00) per Day of the Agreement Term, payable in advance for the first thirty (30) days of the Agreement Term and payable in advance of every Agreement Month thereafter. Agreement Month is defined as thirty (30) consecutive days, commencing on the Delivery date and recurring every thirty (30) days there- after. In all events, CUSTOMER will be charged and CUSTOMER shall pay Daily Rent until the Engine Stand is Redelivered to the redelivery Location in the Redelivery Condition in accordance with Section 14 below. \n\n5. Interest on Past Due Payments: Any payment due hereunder not made within five (5) days after the due date shall accrue interest at the rate of ten percent 10% per annum. \n\n6. Total Loss: It is hereby agreed that if the Engine Stand is lost, destroyed or damaged beyond economic repair, CUSTOMER shall forthwith pay NAS the Agreed Value, in addition to the unpaid Daily Rent, for the Engine Stand. Such payment shall be without setoff for rent or other money paid by CUSTOMER. The Agreed Value for the replacement of the Engine Stand is USS_________ U.S. Dollars (USSI100.000.00). \n\n7. Risk of Loss: Risk of Loss of the Engine Stand passes from NAS to CUSTOMER on the Deliver Date and shall remain with CUSTOMER until the Engine Stand is redelivered in accordance with the provisions of Section 14 below. """,
-                              style: TextStyle(fontSize: 12),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: res_height * 0.01,
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Get.to(lease_form2());
-                    },
-                    child: Container(
-                      width: res_width * 0.9,
-                      decoration: BoxDecoration(
-                          color: Color(0xffaf8a39),
-                          borderRadius: BorderRadius.all(Radius.circular(7))),
-                      child: Padding(
-                        padding: const EdgeInsets.all(13.0),
-                        child: Center(
-                          child: Text(
-                            'Continue',
+            child: Column(
+              children: [
+                Container(
+                  width: res_width * 0.95,
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            "Engine Stand Agreement",
                             style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 17),
+                                fontSize: 15, fontWeight: FontWeight.bold),
                           ),
+                          SizedBox(
+                            height: res_height * 0.01,
+                          ),
+                          Wrap(
+                            spacing: 5,
+                            runSpacing: 10,
+                            alignment: WrapAlignment.start,
+                            children: [
+                              Text(
+                                "This Engine Stand Agreement (the \"Agreement\") is entered this",
+                                style: TextStyle(
+                                    fontSize: 15, fontWeight: FontWeight.w400),
+                              ),
+                              // SizedBox(width: 5),
+                              blank(widt: 60),
+                              Text(
+                                "day of",
+                                style: TextStyle(
+                                    fontSize: 15, fontWeight: FontWeight.w400),
+                              ),
+                              blank(widt: 30),
+                              Text(
+                                """2022 by and between (i) National Aero Stands, LLC, a Texas limited liability company, having its principal place of business at 500 W. 5th Street, Suite 1010, Austin, TX 78701(\"NAS\"), and (ii)""",
+                                style: TextStyle(
+                                    fontSize: 13, fontWeight: FontWeight.w500),
+                              ),
+                              blank(widt: 60),
+                              Text(
+                                "having its principal place of business at",
+                                style: TextStyle(
+                                    fontSize: 15, fontWeight: FontWeight.w400),
+                              ),
+                              blank(widt: 30),
+                              Text(
+                                """("CUSTOMER"). """,
+                                style: TextStyle(fontSize: 12),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: res_height * 0.01,
+                          ),
+                          Text(
+                            "Witnesseth",
+                            style: TextStyle(
+                                fontSize: 15, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: res_height * 0.01,
+                          ),
+                          Wrap(
+                            spacing: 5,
+                            runSpacing: 10,
+                            alignment: WrapAlignment.start,
+                            children: [
+                              Text(
+                                """For sufficient consideration the receipt of which is hereby mutually acknowledged, NAS, as owner, hereby agrees to lease one (1) bootstrap capable engine transportation stand (including accompanying hardware) (herein the foregoing collectively referred to as the "Engine Stand"), as more particularly described below, to CUSTOMER and CUSTOMER agrees to lease the Engine Stand from NAS in accordance with the following basic terms and conditions set forth herein:\n\n 1. Description of Engine Stand: Manufacturer:""",
+                                style: TextStyle(
+                                    fontSize: 13, fontWeight: FontWeight.w500),
+                              ),
+                              blank(widt: 50),
+                              Text(
+                                "Engine Stand Color:",
+                                style: TextStyle(
+                                    fontSize: 13, fontWeight: FontWeight.w500),
+                              ),
+                              blank(widt: 50),
+                              Text(
+                                "Type:",
+                                style: TextStyle(
+                                    fontSize: 13, fontWeight: FontWeight.w500),
+                              ),
+                              blank(widt: 50),
+                              Text(
+                                "Serial No.",
+                                style: TextStyle(
+                                    fontSize: 13, fontWeight: FontWeight.w500),
+                              ),
+                              blank(widt: 50),
+                              Text(
+                                "Quantity:",
+                                style: TextStyle(
+                                    fontSize: 13, fontWeight: FontWeight.w500),
+                              ),
+                              blank(widt: 50),
+                            ],
+                          ),
+                          SizedBox(height: 10),
+                          Wrap(
+                            spacing: 5,
+                            runSpacing: 10,
+                            alignment: WrapAlignment.start,
+                            children: [
+                              Text(
+                                "Delivery: The Engine Stand will be delivered to CUSTOMER ExWorks (Incoterms 2010 Edition) NAS's facility, located in",
+                                style: TextStyle(
+                                    fontSize: 13, fontWeight: FontWeight.w500),
+                              ),
+                              blank(widt: 50),
+                              Text(
+                                """("Delivery Location") by NAS's tender of the Engine Stand to CUSTOMER's common carrier for shipment outside the State of Florida on (the "Delivery Date")""",
+                                style: TextStyle(
+                                    fontSize: 13, fontWeight: FontWeight.w500),
+                              ),
+                              blank(widt: 50),
+                              Text(
+                                "-2022",
+                                style: TextStyle(
+                                    fontSize: 13, fontWeight: FontWeight.w500),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 10),
+                          Wrap(
+                            spacing: 5,
+                            runSpacing: 10,
+                            alignment: WrapAlignment.start,
+                            children: [
+                              Text(
+                                "Agreement Term: This Agreement shall commence on the Delivery Date and shall continue until the date that the Engine Stand has been returned to NAS in accordance with the provisions of Section 14 below (the \"Agreement Term\")",
+                                style: TextStyle(
+                                    fontSize: 13, fontWeight: FontWeight.w500),
+                              ),
+                              blank(widt: 50),
+                              Text(
+                                "\n\n4:Daily Rent: Daily Rent will be charged as follows:",
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                    fontSize: 13, fontWeight: FontWeight.w500),
+                              ),
+                              blank(widt: 50),
+                              Text(
+                                "U.S. Dollars (US.00) per Day of the Agreement Term, payable in advance for the first thirty (30) days of the Agreement Term and payable in advance of every Agreement Month thereafter. Agreement Month is defined as thirty (30) consecutive days, commencing on the Delivery date and recurring every thirty (30) days there- after. In all events, CUSTOMER will be charged and CUSTOMER shall pay Daily Rent until the Engine Stand is Redelivered to the redelivery Location in the Redelivery Condition in accordance with Section 14 below. \n\n5. Interest on Past Due Payments: Any payment due hereunder not made within five (5) days after the due date shall accrue interest at the rate of ten percent 10% per annum. \n\n6. Total Loss: It is hereby agreed that if the Engine Stand is lost, destroyed or damaged beyond economic repair, CUSTOMER shall forthwith pay NAS the Agreed Value, in addition to the unpaid Daily Rent, for the Engine Stand. Such payment shall be without setoff for rent or other money paid by CUSTOMER. The Agreed Value for the replacement of the Engine Stand is USS",
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                    fontSize: 13, fontWeight: FontWeight.w500),
+                              ),
+                              blank(widt: 50),
+                            ],
+                          ),
+                          Text(
+                            """ U.S. Dollars (USSI100.000.00). \n\n7. Risk of Loss: Risk of Loss of the Engine Stand passes from NAS to CUSTOMER on the Deliver Date and shall remain with CUSTOMER until the Engine Stand is redelivered in accordance with the provisions of Section 14 below. """,
+                            style: TextStyle(fontSize: 12),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: res_height * 0.01,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Get.to(lease_form2());
+                  },
+                  child: Container(
+                    width: res_width * 0.9,
+                    decoration: BoxDecoration(
+                        color: Color(0xffaf8a39),
+                        borderRadius: BorderRadius.all(Radius.circular(7))),
+                    child: Padding(
+                      padding: const EdgeInsets.all(13.0),
+                      child: Center(
+                        child: Text(
+                          'Continue',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 17),
                         ),
                       ),
                     ),
                   ),
-                  SizedBox(
-                    height: res_height * 0.01,
-                  ),
-                ],
-              ),
+                ),
+                SizedBox(
+                  height: res_height * 0.01,
+                ),
+              ],
             ),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class blank extends StatelessWidget {
+  TextEditingController? kontroller;
+  double? widt;
+  blank({
+    this.widt,
+    this.kontroller,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: kontroller,
+      decoration: InputDecoration(
+        isCollapsed: true,
+        // contentPadding: EdgeInsets.symmetric(vertical: 0),
+        constraints: BoxConstraints(maxWidth: widt!, minWidth: 10),
+        border: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.grey),
+        ),
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.grey),
+        ),
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.grey),
+        ),
+        disabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.grey),
         ),
       ),
     );
